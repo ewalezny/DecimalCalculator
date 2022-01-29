e4331d05699793c95fd30cff973262f1e4a1429a
ewalezny <ewa.lezny@gmail.com>
2022-01-29 12:04:17 +0100

    changed add method and changeNumber method in decCalc

--- js/DecCalculator.js
+++ js/DecCalculator.js
@@ -1,7 +1,34 @@
 import Calculator from "./Calculator";
 
 class DecCalculator extends Calculator {
+    /**
+     * Method add numbers in two array
+     */
+    add(numberX, numberY) {
+        let result = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+        for (let i = numberX.length - 1; i >= 0; i--) {
+            let sum = numberX[i] + numberY[i] + result[i];
+            if (sum >= 10) {
+                result[i] = sum - 10;
+                result[i - 1] = 1;
+            } else {
+                result[i] = sum;
+            }
+        }
+        return result;
+    }
 
+    /**
+     * Method changing number
+     */
+    changeNumber(parentElement) {
+        const input = parentElement.firstElementChild;
+        input.contentEditable = true;
+        input.focus();
+
+        this.checkNumber();
+        this.updateResult();
+    }
 }
 
 export default DecCalculator;
